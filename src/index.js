cdb258e7012ea36569cfa2476feff43f1acf58f1
lifq <lifq@yunjiglobal.com>
2019-07-31 11:57:11 +0800

    react项目搭建

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/*
+ * @Description: 
+ * @Author: 李方强
+ * @Date: 2019-07-30 17:34:40
+ * @LastEditTime: 2019-07-30 17:37:16
+ * @LastEditors: 李方强
+ */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './views/app'
+
+ReactDOM.render(<App />, document.getElementById('root'))
